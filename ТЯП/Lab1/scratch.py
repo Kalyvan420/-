32454cc25184128be150add44e0eafebf84397e8
Kalyvan420 <47594963+Kalyvan420@users.noreply.github.com>
2019-03-06 01:37:54 +0300

    Add files via upload

---
+++ ТЯП/Lab1/scratch.py
@@ -0,0 +1,6 @@
+a=int(input('Введите число a: '))
+b=int(input('Введите число b: '))
+c=int(input('Введите число c: '))
+d=int(input('Введите число d: '))
+f=int(input('Введите число f: '))
+print(abs(a-b*c*d**3+(c**5-a**2)/a+f**3*(a-213)))
