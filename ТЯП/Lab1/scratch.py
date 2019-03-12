b67f04f3c609c0a463b7077a9a706a1961fa890f
Kalyvan420 <47594963+Kalyvan420@users.noreply.github.com>
2019-03-12 22:04:39 +0300

    Add files via upload

--- ТЯП/Lab1/scratch.py
+++ ТЯП/Lab1/scratch.py
@@ -3,4 +3,7 @@ b=int(input('Введите число b: '))
 c=int(input('Введите число c: '))
 d=int(input('Введите число d: '))
 f=int(input('Введите число f: '))
-print(abs(a-b*c*d**3+(c**5-a**2)/a+f**3*(a-213)))
+if a!=0:
+    print(abs(a-b*c*d**3+(c**5-a**2)/a+f**3*(a-213)))
+else:
+    print('Число а не может ровнятся нулю')
